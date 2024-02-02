e78f086b94c97912eb974d3bfb4038c2b414fe25
Romey-Artcenter <141177160+Romey-Artcenter@users.noreply.github.com>
2024-02-02 10:28:47 -0800

    Update digital_input_output.py

--- class03/digital_input_output.py
+++ class03/digital_input_output.py
@@ -6,25 +6,25 @@ import time
 
 pin41 = None
 pin1 = None
+button_value = None
 
 def setup():
   global pin41, pin1
-
   M5.begin()
-  # initiallize pin 41 (screen button on Atom S3 _ test)
+  # initialize pin 41 (screen button on AtomS3 board) as input:
   pin41 = Pin(41, mode=Pin.IN)
+  # initialize pin 1 (bottom connector white wire) as output:
   pin1 = Pin(1, mode=Pin.OUT)
 
-
 def loop():
-  global pin41, pin1
+  global pin41, pin1, button_value
   M5.update()
-  if pin41.value():
+  button_value = pin41.value()
+  if button_value == 0:
     pin1.on()
   else:
     pin1.off()
   time.sleep_ms(100)
-
 
 if __name__ == '__main__':
   try:
