5db131b9bbfa39537259506348eaeccbd7eb124c
Romey-Artcenter <141177160+Romey-Artcenter@users.noreply.github.com>
2024-02-08 20:17:54 -0800

    Update test01.py

--- class03/test01.py
+++ class03/test01.py
@@ -10,7 +10,6 @@ program_state_label = None
 input_pin = None
 input_value = 0
 input_timer = 0
-
 program_state = 'START'
 
 def setup():
@@ -19,9 +18,9 @@ def setup():
   M5.begin()
   pin_label = Widgets.Label("input", 5, 5, 1.0, 0xffffff, 0x000000, Widgets.FONTS.DejaVu18)
   program_state_label = Widgets.Label("START", 5, 25, 1.0, 0xffffff, 0x000000, Widgets.FONTS.DejaVu18)
-  # initialize input on pin 41 (built-in screen button on AtomS3):
+  # initialize input on pin 41 (built-in button):
   #input_pin = Pin(41, mode=Pin.IN)
-  # initialize input on pin 1 with pull up to make it HIGH by default:
+  # initialize input on pin 1 with pull up:
   input_pin = Pin(1, mode=Pin.IN, pull=Pin.PULL_UP)
 
 def loop():
@@ -32,21 +31,15 @@ def loop():
   M5.update()
   # check input every 500 ms (half second):
   if time.ticks_ms() > input_timer + 500:
-    # update input_timer with current time in milliseconds:
-    input_timer = time.ticks_ms()
-    # assign input_pin value to input_value variable:
+    input_timer = time.ticks_ms()  # update button_timer
     input_value = input_pin.value()
-    # update pin_label text according to input_value:
-    if input_value == 0:              
-      pin_label.setText('input LOW')  
-    else:
-      pin_label.setText('input HIGH') 
-  # condition to check that the program is in START state:
-  if program_state == 'START':
-    # condition for changing program state to RUN:
     if input_value == 0:
-      program_state = 'RUN'  
-      program_state_label.setText('RUN')
+      pin_label.setText('pin ON')
+      if program_state == 'START':
+        program_state = 'RUN'
+        program_state_label.setText(program_state)
+    else:
+      pin_label.setText('pin OFF')
   
 if __name__ == '__main__':
   try:
