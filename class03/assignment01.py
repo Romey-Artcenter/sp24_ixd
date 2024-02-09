5f952c19614a20d85eac27976326059473d737e3
Romey-Artcenter <141177160+Romey-Artcenter@users.noreply.github.com>
2024-02-09 09:49:58 -0800

    Update assignment01.py

--- class03/assignment01.py
+++ class03/assignment01.py
@@ -10,44 +10,48 @@ program_state_label = None
 input_pin = None
 input_value = 0
 input_timer = 0
+rgb2 = None
+label1 = None
 
 program_state = 'START'
 
 def setup():
   global pin_label, program_state_label, input_pin
-
+  global rgb2
+  global label1
   M5.begin()
-  pin_label = Widgets.Label("input", 5, 5, 1.0, 0xffffff, 0x000000, Widgets.FONTS.DejaVu18)
-  program_state_label = Widgets.Label("START", 5, 25, 1.0, 0xffffff, 0x000000, Widgets.FONTS.DejaVu18)
-  # initialize input on pin 41 (built-in screen button on AtomS3):
-  #input_pin = Pin(41, mode=Pin.IN)
-  # initialize input on pin 1 with pull up to make it HIGH by default:
+  
+  #input_pin = Pin(39, mode=Pin.IN, pull=Pin.PULL_UP)
+  
+  # initialize input on pin 1
   input_pin = Pin(1, mode=Pin.IN, pull=Pin.PULL_UP)
+  
+  #initialize RGB LED strip on pin 38 with 10 pixels
+  rgb2 = RGB(io=38, n=10, type="SK6812")
+  #0xFFFFF 폰트 컬러, 0x000000 디스플레이 백그라운드 컬
+  program_state_label = Widgets.Label("START", 5, 25, 1.0, 0xffffff, 0x000000, Widgets.FONTS.DejaVu18)
 
 def loop():
   global pin_label, program_state_label
   global input_value
   global input_timer
   global program_state
+  global rgb2
+  global label1
   M5.update()
-  # check input every 500 ms (half second):
   if time.ticks_ms() > input_timer + 500:
-    # update input_timer with current time in milliseconds:
     input_timer = time.ticks_ms()
-    # assign input_pin value to input_value variable:
     input_value = input_pin.value()
-    # update pin_label text according to input_value:
-    if input_value == 0:              
-      pin_label.setText('input LOW')  
-    else:
-      pin_label.setText('input HIGH') 
-  # condition to check that the program is in START state:
-  if program_state == 'START':
-    # condition for changing program state to RUN:
     if input_value == 0:
-      program_state = 'RUN'  
-      program_state_label.setText('RUN')
-  
+      rgb2.fill_color(0x0dff25)
+    else:
+      #Widgets.fillScreen(0xffffff)
+      rgb2.fill_color(0xffffff)
+  if program_state == 'START':
+    if input_value == 0:
+      program_state = 'RUN'
+      program_state_label.setText('RUN,   ')
+      
 if __name__ == '__main__':
   try:
     setup()
@@ -59,3 +63,6 @@ if __name__ == '__main__':
       print_error_msg(e)
     except ImportError:
       print("please update to latest firmware")
+
+
+
